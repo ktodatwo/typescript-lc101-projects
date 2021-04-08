2163e147199bbec1e75704b61208f560a1dc1f16
Katherine <katherine@katherines-mbp.lan>
2021-04-08 13:04:44 -0500

    initial commit

--- exercises/SpaceLocation.ts
+++ exercises/SpaceLocation.ts
@@ -1,1 +1,16 @@
 // Paste in the provided code here:
+
+export class SpaceLocation {
+    kilometersAway: number;
+    name: string;
+ 
+    constructor(name: string, kilometersAway: number) {
+       this.name = name;
+       this.kilometersAway = kilometersAway;
+    }
+
+    
+
+
+
+ }
